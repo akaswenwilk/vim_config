901ef5d055a42ecf46b6ab7add8c27d589fe1070
Aaron Kaswen-Wilk <aaron.kaswen-wilk@qonto.com>
2025-06-18 11:56:43 +0200

    grugfar and avsc filetype

---
+++ ftplugin/avsc.lua
@@ -0,0 +1,7 @@
+local opt = vim.opt
+local keymap = vim.keymap.set
+
+keymap('n', '<leader>j', [[:%!jq .<CR>]], { noremap = true, silent = true, desc = "Prettify JSON with jq" })
+
+opt.shiftwidth = 2
+opt.softtabstop = 2
